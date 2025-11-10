22705994cca86c2892e2e6227a716622a951e326
sachinkumar <sachinkumar5035@gmail.com>
2025-11-10 23:07:53 +0530

    comment added

--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -6,7 +6,7 @@ public class Employee {
     private String address;
     private Double salary;
 
-    // Getters and Setters
+    // Getters and Setters added
     public Integer getEmpId() { return empId; }
     public void setEmpId(Integer empId) { this.empId = empId; }
 
